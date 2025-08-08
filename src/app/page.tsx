67c5cd512a85767918ed5fb77c2b234d8b22a1f4
himanimanjunath <himanimanjunath05@gmail.com>
2025-08-07 21:57:46 -0700

    added logo to header + updated header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Badge, Leaf } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,26 +7,35 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white">
 
-      {/*header first*/}
+      {/*Header*/}
 
       <header className="bg-white shadow-sm border-b">
-        <div className="flex">
-          
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center h-25">
+            <div>
+              <img src="logo.png" width={90} height={90} alt='logo' href="null"></img>
+
+            </div>
             <nav className="hidden md:flex space-x-7">
-              <Link className="text-gray-700 hover:text-orange-400 font-medium" href="#menu">Menu</Link>
+              <Link className="= text-gray-700 hover:text-orange-400 font-medium" href="#menu">Menu</Link>
               <Link className="text-gray-700 hover:text-orange-400 font-medium" href="#about">About</Link>
               <Link className="text-gray-700 hover:text-orange-400 font-medium" href="#contact">Contact</Link>
             </nav>
             <div className="flex space-x-4">
-              <Button asChild className="bg-orange-400 hover:bg-orange-500">
+              <Button asChild className="bg-orange-400 hover:bg-orange-500 h-12 w-30">
                 <Link href="https://www.clover.com/online-ordering/masala-dosa-milpitas" target="_blank">
                   Order Online
                 </Link>
               </Button>
             </div>
+          </div>
         </div>
       </header>
 
+      {/*Hero*/}
+      <section>
+
+      </section>
       
     </div>
   );
